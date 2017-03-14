650077af5f58ed6b7e1f6273aa4fd2ae87b156b7
Neylson Crepalde <neylsoncrepalde@gmail.com>
2017-03-14 16:06:10 -0300

    Create econometria1_aula2.py

---
+++ Python/econometria1_aula2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Econometria 1
+Profa. Sueli Moro
+
+@author: Neylson Crepalde
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+os.chdir('/home/neylson/Documentos/Neylson Crepalde/Doutorado/Econometria1/AULAS 2017')
+
+dados = pd.read_stata('espuria.dta') #Funcionando
