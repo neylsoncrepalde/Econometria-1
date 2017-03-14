142b29c19207e48913ea954455dd8f7266b1d149
Neylson Crepalde <neylsoncrepalde@gmail.com>
2017-03-14 16:06:44 -0300

    Create econometria1_aula2.R

---
+++ R/econometria1_aula2.R
@@ -0,0 +1,13 @@
+# Aula de Econometria 1
+# Profa. Sueli Moro
+# Neylson Crepalde
+#########################
+
+# Aula 2 - 14/03/2017
+#####################
+
+setwd('~/Documentos/Neylson Crepalde/Doutorado/Econometria1/AULAS 2017')
+
+library(foreign)
+dados = read.dta('espuria.dta') #Funcionando
+
